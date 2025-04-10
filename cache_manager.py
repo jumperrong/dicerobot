669c3f05b0bd3602c48b888a13427fe8773a44ba
jumperrong <jumper.rong@outlook.com>
2025-04-10 18:08:59 +0800

    移除摸鱼日历，api经常无法下载图片

--- cache_manager.py
+++ cache_manager.py
@@ -18,16 +18,14 @@ class CacheManager:
         os.makedirs(self.cache_root, exist_ok=True)
         
         # 各功能缓存目录
-        self.moyu_dir = os.path.join(self.cache_root, 'moyu')
         self.jrrp_dir = os.path.join(self.cache_root, 'jrrp')
         self.weather_dir = os.path.join(self.cache_root, 'weather')
         
         # 创建各功能缓存目录
-        for cache_dir in [self.moyu_dir, self.jrrp_dir, self.weather_dir]:
+        for cache_dir in [self.jrrp_dir, self.weather_dir]:
             os.makedirs(cache_dir, exist_ok=True)
             
         # 各功能缓存文件路径
-        self.moyu_broadcast_file = os.path.join(self.moyu_dir, 'broadcast_history.json')
         self.jrrp_broadcast_file = os.path.join(self.jrrp_dir, 'broadcast_history.json')
         self.weather_broadcast_file = os.path.join(self.weather_dir, 'broadcast_history.json')
         
@@ -38,7 +36,7 @@ class CacheManager:
         """加载指定功能的播报历史记录
         
         Args:
-            feature: 功能名称 ('moyu', 'jrrp', 'weather')
+            feature: 功能名称 ('jrrp', 'weather')
             
         Returns:
             Dict: 播报历史记录
@@ -76,8 +74,7 @@ class CacheManager:
         """保存指定功能的播报历史记录
         
         Args:
-            feature: 功能名称 ('moyu', 'jrrp', 'weather')
-            data: 要保存的数据
+            feature: 功能名称 ('jrrp', 'weather')
             
         Returns:
             bool: 是否保存成功
@@ -113,7 +110,7 @@ class CacheManager:
         """获取指定功能的图片缓存目录
         
         Args:
-            feature: 功能名称 ('moyu', 'jrrp', 'weather')
+            feature: 功能名称 ('jrrp', 'weather')
             
         Returns:
             str: 图片缓存目录路径
@@ -139,7 +136,7 @@ class CacheManager:
         """清理指定功能的过期缓存文件
         
         Args:
-            feature: 功能名称 ('moyu', 'jrrp', 'weather')
+            feature: 功能名称 ('jrrp', 'weather')
             days: 过期天数
         """
         try:
